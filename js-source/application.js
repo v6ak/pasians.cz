9be1a0a6a877991ab651fd6a2a58e09e55dcbe56
Your Name <you@example.com>
2024-12-24 01:12:09 +0100

    Change default theme to something where suit is always visible

--- js-source/application.js
+++ js-source/application.js
@@ -627,7 +627,7 @@ const CREDITS_HRACIKARTY = '<a href="https://www.hracikarty.cz/" target="_blank"
 		},
 
 		current: null,
-		defaultTheme: "rummy-senior",
+		defaultTheme: "bridge-rummy",
 
 		/* theres no mechanism yet to load the appropriate deck depending on the scaled card width
 		 * so we just load the largest cards and call it a day :/
