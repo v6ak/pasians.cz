bf4f5f7ef8c974b3b18cf8765a692cc0b1afe402
Your Name <you@example.com>
2025-01-03 12:42:18 +0100

    Rename js-source to src-yui-legacy-modules

--- src/modern.ts
+++ src/modern.ts
@@ -1,20 +1,20 @@
 import './yui-loader';
 
-import '../js-source/extend-array.js';
-import '../js-source/extend-function.js';
-import '../js-source/extend-object.js';
+import '../src-yui-legacy-modules/extend-array.js';
+import '../src-yui-legacy-modules/extend-function.js';
+import '../src-yui-legacy-modules/extend-object.js';
 
-import '../js-source/analytics.js';
-import '../js-source/solitaire.js';
-import '../js-source/win-display.js';
-import '../js-source/auto-stack-clear.js';
-import '../js-source/auto-turnover.js';
-import '../js-source/autoplay.js';
-import '../js-source/ie-opera-background-fix.js';
-import '../js-source/statistics.js';
-import '../js-source/klondike.js';
-import '../js-source/klondike1t.js';
-import '../js-source/spider.js';
-import '../js-source/spider1s.js';
-import '../js-source/spider2s.js';
-import '../js-source/application.js';
+import '../src-yui-legacy-modules/analytics.js';
+import '../src-yui-legacy-modules/solitaire.js';
+import '../src-yui-legacy-modules/win-display.js';
+import '../src-yui-legacy-modules/auto-stack-clear.js';
+import '../src-yui-legacy-modules/auto-turnover.js';
+import '../src-yui-legacy-modules/autoplay.js';
+import '../src-yui-legacy-modules/ie-opera-background-fix.js';
+import '../src-yui-legacy-modules/statistics.js';
+import '../src-yui-legacy-modules/klondike.js';
+import '../src-yui-legacy-modules/klondike1t.js';
+import '../src-yui-legacy-modules/spider.js';
+import '../src-yui-legacy-modules/spider1s.js';
+import '../src-yui-legacy-modules/spider2s.js';
+import '../src-yui-legacy-modules/application.js';
