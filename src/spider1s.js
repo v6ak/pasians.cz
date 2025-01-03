d7037d69e4df0a98b636aa765762c1af8563e725
Your Name <you@example.com>
2025-01-03 17:56:46 +0100

    fixup! Spider: Migrate to ES modules

--- src/spider1s.js
+++ src/spider1s.js
@@ -1,8 +1,5 @@
 import { instance } from "./util";
 import { Spider } from "./spider";
-import { use } from "./yui-modules-interop";
-
-const Y = use("spider");
 
 export const Spider1S = instance(Spider, {name: () => 'Spider1S'});
 Spider1S.Deck = instance(Spider1S.Deck, {
--- src/spider2s.js
+++ src/spider2s.js
@@ -1,8 +1,5 @@
 import { instance } from "./util";
 import { Spider } from "./spider";
-import { use } from "./yui-modules-interop";
-
-const Y = use("spider");
 
 export const Spider2S = instance(Spider, {name: () => 'Spider2S'});
 Spider2S.Deck = instance(Spider2S.Deck, {
